41c80b09d42261932eb135c64948290f1f3a3b2f
Başak Cengiz <120058178+basakcengiz@users.noreply.github.com>
2024-10-09 11:24:13 +0300

    sidebar updated

--- basak-portfolio/src/components/Home.tsx
+++ basak-portfolio/src/components/Home.tsx
@@ -10,35 +10,35 @@ import yarn from '../assets/yarn.png';
 import graphql from '../assets/graphql.png';
 
 const Home = () => {
-  return (
-    <div className="h-screen ">
-      <div className="flex flex-col items-center justify-evenly sm:flex-row h-3/5 mt-20 gap-2">
-        <div>
-          <img src={bc} alt="bc" className="rounded-full w-56 h-72 border-[#b1b1b1] border-2 hover:scale-125 duration-300 ease-in-out" />
-        </div>
-        <div className="flex flex-col items-center gap-4">
-          <span className="font-bold text-3xl rounded-lg p-4 text-white bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
-            HI, I AM BASAK CENGIZ! 👋🏻
-          </span>
-          <span>Frontend Developer 💻</span>
-          <span className="border-b-2 border-[#764ABC]">Tech Stack</span>
-          <div>
-            <div className="flex flex-wrap gap-2">
-              <img src={html} alt="html" className="w-10 h-10" />
-              <img src={css} alt="css" className="w-10 h-10" />
-              <img src={js} alt="js" className="w-10 h-10" />
-              <img src={ts} alt="ts" className="w-10 h-10" />
-              <img src={react} alt="react" className="w-10 h-10" />
-              <img src={redux} alt="redux" className="w-10 h-10" />
-              <img src={tailwind} alt="tailwind" className="w-10 h-10" />
-              <img src={graphql} alt="graphql" className="w-10 h-10" />
-              <img src={yarn} alt="yarn" className="w-10 h-10" />
+   return (
+      <div className="h-screen ">
+         <div className="flex flex-col items-center gap-2 mt-20 justify-evenly sm:flex-row h-3/5">
+            <div>
+               <img src={bc} alt="bc" className="rounded-full w-56 h-72 border-[#b1b1b1] border-2 md:hover:scale-125 duration-300 ease-in-out" />
             </div>
-          </div>
-        </div>
+            <div className="flex flex-col items-center gap-4">
+               <span className="font-bold text-3xl rounded-lg p-4 text-white bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+                  HI, I AM BASAK CENGIZ! 👋🏻
+               </span>
+               <span>Frontend Developer 💻</span>
+               <span className="border-b-2 border-[#764ABC]">Tech Stack</span>
+               <div>
+                  <div className="flex flex-wrap justify-center gap-2">
+                     <img src={html} alt="html" className="w-10 h-10" />
+                     <img src={css} alt="css" className="w-10 h-10" />
+                     <img src={js} alt="js" className="w-10 h-10" />
+                     <img src={ts} alt="ts" className="w-10 h-10" />
+                     <img src={react} alt="react" className="w-10 h-10" />
+                     <img src={redux} alt="redux" className="w-10 h-10" />
+                     <img src={tailwind} alt="tailwind" className="w-10 h-10" />
+                     <img src={graphql} alt="graphql" className="w-10 h-10" />
+                     <img src={yarn} alt="yarn" className="w-10 h-10" />
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Home;
--- basak-portfolio/src/components/Sidebar.tsx
+++ basak-portfolio/src/components/Sidebar.tsx
@@ -2,37 +2,37 @@ import CloseIcon from '@mui/icons-material/Close';
 import { links } from '../helpers/arrays';
 import { LinkType } from '../helpers/types';
 type Props = {
-  setSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+   setSidebar: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const Sidebar = ({ setSidebar }: Props) => {
-  return (
-    <div className="h-screen w-full border flex flex-col">
-      <div
-        className="flex justify-end p-6 cursor-pointer"
-        onClick={() => {
-          setSidebar(false);
-        }}
-      >
-        <CloseIcon />
+   return (
+      <div className="fixed inset-0 bg-[#FAF7F0] z-50 flex flex-col w-full h-screen border ">
+         <div
+            className="flex justify-end p-6 cursor-pointer"
+            onClick={() => {
+               setSidebar(false);
+            }}
+         >
+            <CloseIcon />
+         </div>
+         <div className="flex flex-col items-center justify-between mt-40 h-2/5 ">
+            {links.map((link: LinkType) => {
+               return (
+                  <a
+                     href={link.id}
+                     key={link.id}
+                     onClick={() => {
+                        setSidebar(false);
+                     }}
+                  >
+                     {link.name}
+                  </a>
+               );
+            })}
+         </div>
       </div>
-      <div className="flex flex-col items-center justify-between h-2/5 mt-40 ">
-        {links.map((link: LinkType) => {
-          return (
-            <a
-              href={link.id}
-              key={link.id}
-              onClick={() => {
-                setSidebar(false);
-              }}
-            >
-              {link.name}
-            </a>
-          );
-        })}
-      </div>
-    </div>
-  );
+   );
 };
 
 export default Sidebar;
