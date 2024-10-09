bcffc6f5458f073dcbce72f2266452003643d222
Başak Cengiz <120058178+basakcengiz@users.noreply.github.com>
2024-10-09 12:28:58 +0300

    cv added

--- basak-portfolio/src/components/Home.tsx
+++ basak-portfolio/src/components/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
       <div className="h-screen ">
          <div className="flex flex-col items-center gap-2 mt-20 justify-evenly sm:flex-row h-3/5">
             <div>
-               <img src={bc} alt="bc" className="rounded-full w-56 h-72 border-[#b1b1b1] border-2 md:hover:scale-125 duration-300 ease-in-out" />
+               <img src={bc} alt="bc" className="rounded-full w-68 max-h-96 border-[#b1b1b1] border-2 md:hover:scale-110 duration-300 ease-in-out" />
             </div>
             <div className="flex flex-col items-center gap-4">
                <span className="font-bold text-3xl rounded-lg p-4 text-white bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
@@ -22,7 +22,7 @@ const Home = () => {
                </span>
                <span>Frontend Developer 💻</span>
                <span className="border-b-2 border-[#764ABC]">Tech Stack</span>
-               <div>
+               <div className="flex flex-col items-center justify-center gap-6">
                   <div className="flex flex-wrap justify-center gap-2">
                      <img src={html} alt="html" className="w-10 h-10" />
                      <img src={css} alt="css" className="w-10 h-10" />
@@ -33,6 +33,16 @@ const Home = () => {
                      <img src={tailwind} alt="tailwind" className="w-10 h-10" />
                      <img src={graphql} alt="graphql" className="w-10 h-10" />
                      <img src={yarn} alt="yarn" className="w-10 h-10" />
+                  </div>
+                  <div className="flex items-center justify-center w-1/3 h-8">
+                     <a
+                        href="/BASAK_CENGIZ_CV.pdf"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="flex items-center justify-center w-full h-full text-white rounded-full bg-sky-400"
+                     >
+                        View CV
+                     </a>
                   </div>
                </div>
             </div>
