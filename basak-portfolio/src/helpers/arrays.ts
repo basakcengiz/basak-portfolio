745406992ca61ec2e38a6f70173641d87c009a5b
Başak Cengiz <120058178+basakcengiz@users.noreply.github.com>
2024-10-10 10:20:57 +0300

    comment removed

--- basak-portfolio/src/helpers/arrays.ts
+++ basak-portfolio/src/helpers/arrays.ts
@@ -1,24 +1,20 @@
 import { LinkType } from './types';
 
 export const links: LinkType[] = [
-  {
-    name: 'About',
-    id: '#about'
-  },
-  {
-    name: 'Projects',
-    id: '#projects'
-  },
-  /*  {
-    name: 'Skills',
-    id: '#skills'
-  }, */
-  {
-    name: 'Contact',
-    id: '#contact'
-  },
-  {
-    name: 'Gallery',
-    id: '#gallery'
-  }
+   {
+      name: 'About',
+      id: '#about',
+   },
+   {
+      name: 'Projects',
+      id: '#projects',
+   },
+   {
+      name: 'Contact',
+      id: '#contact',
+   },
+   {
+      name: 'Gallery',
+      id: '#gallery',
+   },
 ];
